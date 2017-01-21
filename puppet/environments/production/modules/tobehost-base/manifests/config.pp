51e6b569d836e92af7591b528e9565f035d4e860
Mathieu Pellegrin <mpellegrin@users.noreply.github.com>
2017-01-21 17:07:10 +0100

    Add cronjob to bridge Drupal and Hiera JSON

--- puppet/environments/production/modules/tobehost-base/manifests/config.pp
+++ puppet/environments/production/modules/tobehost-base/manifests/config.pp
@@ -24,4 +24,19 @@ class tobehost::base::config inherits tobehost::base {
 		mode    => '750',
 	}
 
+	file {'/etc/cron.d/tobehost_panel':
+		ensure  => 'file',
+		content => '*/10 * * * *  root  [ -x /usr/local/sbin/tobehost_panel_cron ] && /usr/local/sbin/tobehost_panel_cron',
+		owner   => '0',
+		group   => '0',
+		mode    => '644',
+	}
+	file {'/usr/local/sbin/tobehost_panel_cron':
+		ensure => 'file',
+		source => 'puppet:///modules/tobehost-base/cron.d/tobehost_panel',
+		owner  => '0',
+		group  => '0',
+		mode   => '750',
+	}
+
 }
